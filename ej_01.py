3efa6cb56a0149a332f74dcc0b2a9dbe0f9df8d2
ndevbogado <ndevbogado@gmail.com>
2023-03-09 16:06:07 -0300

    feat: Add example 01 file

---
+++ ej_01.py
@@ -0,0 +1,7 @@
+# Hallar el valor de la superficie de un cuadrado dado su lado
+
+lado=input("Ingrese la longitud del lado del cuadrado: ")
+lado=int(lado)
+superficie=lado*lado
+superficie=str(superficie)
+print("La superficie del cuadrado es: "+superficie)
