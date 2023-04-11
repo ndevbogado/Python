350761c7ca5df579a6660abc0d1e14fbf9bddeb1
ndevbogado <ndevbogado@gmail.com>
2023-04-11 12:37:38 -0300

    feat: Add tuples and dictionaries
    Practice related to tuples and dictionaries
    Exercise from Python_101

--- dataStructures.py
+++ dataStructures.py
@@ -1,18 +1,35 @@
-"""initialize a list """
-
+"""
 myList = [1, 2, 3]
 myOtherList = [4, 5]
 
 myList.extend(myOtherList)
 print(myList)
-
+"""
+myList = list()
+myDict = dict()
 continueLoop = True
 while continueLoop:
-    item = input("Write some content for the next item to add to the list: (type ´no´ to exit) ")
-    if item.upper() == "NO" or item.upper() == "N":
+    print("""TYPE "NO" TO EXIT""")
+    key = input("Write a key for a key-value pair: ")
+    if key == "":
+        print("Please write a none-zero key...")
+        continue
+
+    value = input("Write some content for the next item to add to the list:")
+
+    if value.upper() == "NO" or value.upper() == "N" or key.upper() == "NO" or key.upper() == "N":
         continueLoop = False
     else:
-        aux = [item]
-        myList.extend(aux)
+        #aux = [int(value)]
+        myDict[key]= value
 
-print(myList)
+#print(myList)
+print(myDict.keys())
+auxList = myDict.items()
+commanders = []
+for commander in auxList:
+    commanders.extend([commander[1]])
+print(commanders)
+#myList.sort()
+#print(myList)
+
